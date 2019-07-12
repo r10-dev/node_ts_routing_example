701bcbf9731d9c8eb97bc326ddbe78e3f4b9ada5
Erick <38711500+elrDevelopment@users.noreply.github.com>
2019-07-12 10:15:50 -0500

    update user routes

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     console.log("user requested");
+    // Make some call to user.controller.ts here
     res.json({user: "erick", fullname: "Erick Roberts"});
 });
 
